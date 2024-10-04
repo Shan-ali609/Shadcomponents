c07484aea9e83f858e39017dba959e439f198297
Shan Ali <shan.softwareengineer007@gmail.com>
2024-10-04 05:58:04 -0700

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,19 +5,19 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    // <div className="py-6 border-t-2 px-4 sm:px-6 md:px-11 text-gray-400 text-center md:text-left">
-    //   <p className="text-sm sm:text-base md:text-base">
-    //     Built by{' '}
-    //     <Link href="#" className="text-gray-500 underline underline-offset-8">
-    //       shadcn
-    //     </Link>
-    //     . The source code is available on{' '}
-    //     <Link href="#" className="text-gray-500 underline underline-offset-8">
-    //       GitHub
-    //     </Link>
-    //     .
-    //   </p>
-    // </div>
-    <div className='hidden'>good</div>
+    <div className="py-6 border-t-2 px-4 sm:px-6 md:px-11 text-gray-400 text-center md:text-left">
+      <p className="text-sm sm:text-base md:text-base">
+        Built by{' '}
+        <Link href="#" className="text-gray-500 underline underline-offset-8">
+          shadcn
+        </Link>
+        . The source code is available on{' '}
+        <Link href="#" className="text-gray-500 underline underline-offset-8">
+          GitHub
+        </Link>
+        .
+      </p>
+    </div>
+    
   );
 }
