fb832921f883cacc646ced40b6fbe611a8420819
Shan Ali <shan.softwareengineer007@gmail.com>
2024-10-05 03:41:06 -0700

    bbb

--- src/components/leftcomp/Leftside.jsx
+++ src/components/leftcomp/Leftside.jsx
@@ -1,30 +1,4 @@
 
-// "use client"
-// import React , {useState}from "react";
-// import '@/components/leftcomp/leftside.css';
-// import Link from "next/link";
-// import Datacomp from "../pagecomp/Datacomp";
-// export default function Leftside() {
-//   const [selectedid , setselectedid ] = useState(null);
-  
-//   return (
-//     <div className="left-container dark:bg-black">
-//       <ul className="left-ul scroll-container">
-//         {Datacomp.map((data) => (
-//           <li key={data.id} 
-//           onClick={()=>setselectedid(data.id)}
-//           className= {`left-li  ${selectedid === data.id ? "shan" : ""}`}
-//           >
-//             <Link className=" raleway  left-a dark:text-white " href={`/component/${data.id}` }>
-//               {data.name}
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
 "use client";
 import React, { useState, useEffect } from "react";
 import '@/components/leftcomp/leftside.css';
@@ -33,8 +7,6 @@ import Datacomp from "../pagecomp/Datacomp";
 
 export default function Leftside() {
   const [selectedid, setselectedid] = useState(null);
-
-  // Load the selected id from localStorage when the component mounts
   useEffect(() => {
     const storedId = localStorage.getItem("selectedid");
     if (storedId) {
@@ -42,10 +14,9 @@ export default function Leftside() {
     }
   }, []);
 
-  // Update the selected id and save it to localStorage
   const handleSelect = (id) => {
     setselectedid(id);
-    localStorage.setItem("selectedid", id); // Save selected id to localStorage
+    localStorage.setItem("selectedid", id); 
   };
 
   return (
