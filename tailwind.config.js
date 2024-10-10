f27e85319650fe4eb37cc26757705bd1b36e95ab
Shan Ali <shan.softwareengineer007@gmail.com>
2024-10-10 01:17:03 -0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,3 @@
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-// 	darkMode: "class",
-// 	content: [
-// 	  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-// 	  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-// 	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-// 	],
-// 	theme: {
-// 	  darkMode: "class",
-// 	  extend: {
-// 		colors: {
-// 		  background: "var(--background)",
-// 		  foreground: "var(--foreground)",
-// 		},
-// 		borderRadius: {
-// 		  lg: "var(--radius)",
-// 		  md: "calc(var(--radius) - 2px)",
-// 		  sm: "calc(var(--radius) - 4px)",
-// 		},
-// 		fontFamily: {
-// 		  raleway: ['var(--font-raleway)', 'sans-serif'], 
-// 		  roboto: ['var(--font-roboto)', 'sans-serif'],   
-// 		},
-// 	  },
-// 	},
-// 	plugins: [require("tailwindcss-animate")],
-//   };
-  
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: "class",
@@ -38,12 +7,11 @@ module.exports = {
 	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+	  darkMode: "class",
 	  extend: {
 		colors: {
 		  background: "var(--background)",
 		  foreground: "var(--foreground)",
-		  'scrollbar-bg': '#ffffff',
-		  'scrollbar-hover': '#e9e9e9',
 		},
 		borderRadius: {
 		  lg: "var(--radius)",
@@ -52,25 +20,13 @@ module.exports = {
 		},
 		fontFamily: {
 		  raleway: ['var(--font-raleway)', 'sans-serif'], 
-		  roboto: ['var(--font-roboto)', 'sans-serif'],  
+		  roboto: ['var(--font-roboto)', 'sans-serif'],   
 		},
 	  },
 	},
-	plugins: [
-	  require("tailwindcss-animate"),
-	  function ({ addUtilities }) {
-		addUtilities({
-		  '.scrollbar-thin': {
-			'scrollbar-width': 'thin',
-		  },
-		  '.scrollbar-thumb-white': {
-			'scrollbar-color': 'white white',
-		  },
-		  '.scrollbar-thumb-hover': {
-			'scrollbar-color': '#e9e9e9 white',
-		  },
-		});
-	  },
-	],
+	plugins: [require("tailwindcss-animate")],
   };
+  
+
+
   
