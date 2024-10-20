3b8847424893d45e63f7e2758cc8e916c45f625e
Shan Ali <shan.softwareengineer007@gmail.com>
2024-10-20 00:45:13 -0700

    pk

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,4 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import Accordion from "@/components/pagecomp/Accordion";
-
 export default function Home() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/component/accordion" />} />
-          <Route path="/component/accordion" element={<Accordion />} />
-        </Routes>
-      </Router>
-    </>
-  );
+  return <></>;
 }
