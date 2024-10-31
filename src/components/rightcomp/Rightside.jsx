a0d7d9c4cd9ff2e1b5e82d4e34124a8cd0dd97ea
Shan Ali <shan.softwareengineer007@gmail.com>
2024-10-31 02:14:58 -0700

    shadcn

--- src/components/rightcomp/Rightside.jsx
+++ src/components/rightcomp/Rightside.jsx
@@ -18,20 +18,20 @@ export default function Rightside() {
           <Link href="">Usage</Link>
         </li>
       </ul>
-      <div className="border w-[250px] h-[260px] rounded-md flex flex-col p-2 gap-2  ">
-        <p className="font-sans font-bold text-[18px] group-hover:underline cursor-pointer  w-[160px] hover:underline">
+      <div className="border dark:border-white/40 w-[250px] h-[260px] rounded-md flex flex-col p-2 gap-2  ">
+        <p className="font-sans font-bold dark:text-white text-[18px] group-hover:underline cursor-pointer  w-[160px] hover:underline">
           Bring your app built with shadcn to life on vercel
         </p>
-        <p className="text-[13px] font-sans w-[170px]">
+        <p className="text-[13px] font-sans w-[170px] dark:text-white">
           Trusted by OpenAI, Sonos, Chick-fil-A, and more.
         </p>
-        <p className="text-[13px] w-[190px] font-sans">
+        <p className="text-[13px] w-[190px] font-sans dark:text-white">
           Vercel provides tools and infrastructure to deploy apps and features
           at scale.
         </p>
         <button
           type="button"
-          className="bg-black text-white text-[12px] w-[100px] p-2 rounded-md"
+          className="bg-black dark:bg-white dark:text-black text-white text-[12px] w-[100px] p-2 rounded-md"
         >
           Deploy Now
         </button>
